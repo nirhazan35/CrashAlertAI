c1d6a82aba7b23fff11896eb9c470c51776aa72f
darf10 <118679710+darf10@users.noreply.github.com>
2024-12-17 16:49:50 +0200

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+const url = "http://localhost:3001"
 
 function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:3001")
+    axios.get(`${url}`)
       .then((res) => setMessage(res.data))
       .catch((err) => console.error(err));
   }, []);
