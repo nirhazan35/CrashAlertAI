e40a0ed6e4c1c8071083fcb6af3d2d3da0f4fd57
darf10 <118679710+darf10@users.noreply.github.com>
2024-12-17 16:07:11 +0200

    basic setup 1

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json('Test' );
+});
+app.listen(port, () => {
+    console.log(`Backend server running at http://localhost:${port}`);
+});
