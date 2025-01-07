39c6f7537d08328a8c54899436dc5e64b3ffbdcc
darf10 <118679710+darf10@users.noreply.github.com>
2025-01-07 15:14:21 +0200

    generating token.

--- backend/util/generateToken.js
+++ backend/util/generateToken.js
@@ -4,12 +4,11 @@ const jwt = require('jsonwebtoken');
  * Generates a JSON Web Token for a user.
  * @param {Object} payload - The payload to include in the token (user ID).
  * @param {String} secret - The JWT secret key.
- * @param {Object} options - Additional options for token generation (e.g., expiry).
+ * @param {Object} options - Additional options for token generation (expiry).
  * @returns {String} - The signed JWT.
  */
-const generateToken = (payload, secret, options = {}) => {
+const generateToken = (payload, secret=process.env.JWT_SECRET, options = {}) => {
   return jwt.sign(payload, secret, options);
 };
 
 module.exports = generateToken;
-
