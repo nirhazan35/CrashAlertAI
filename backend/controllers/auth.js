0192b76ce76276ebb7694cbe7242346d7a7ebee6
Nir Hazan <nirhazan7@gmail.com>
2025-01-12 15:51:56 +0200

    Enhance authentication logging: add logging for user registration, login, and logout actions, and update role retrieval method for consistency

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -8,8 +8,10 @@ const authLogs = require("../models/AuthLogs");
 // Register
 const register = async (req, res) => {
   try {
+    const adminUsername = (await User.findById( req.user.id )).get('username');
+    const authLog = new authLogs();
+    await authLog.initializeAndSave(adminUsername, "Register"); // Initialize and save the log
     const { username, email, password, role } = req.body;
-
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
@@ -29,24 +31,19 @@ const register = async (req, res) => {
     }
 
     const savedUser = await newUser.save();
+    await authLog.updateResult("Success"); // Update the log result to "Success"
     res.status(201).json(savedUser);
   } catch (error) {
     res.status(500).json({ error: "Failed to create user", message: error.message });
   }
-
 };
 
 // Login
 const login = async (req, res) => {
-  const authLog = new authLogs({
-    username: req.body.username || "Unknown", // Use provided username or "Unknown" if undefined
-    type: "Login",
-    result: "Failure", // Default to "Failure" and update later if successful
-  });
-
   try {
     const { username, password } = req.body;
-
+    const authLog = new authLogs();
+    await authLog.initializeAndSave(username, "Login"); // Initialize and save the log
     const user = await User.findOne({ username });
     if (!user) {
 
@@ -64,35 +61,25 @@ const login = async (req, res) => {
       return res.status(400).json({ error: "Invalid password." });
     }
     const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-      // Store token in cookie
-    res.cookie('token', token, { httpOnly: true});
+    await authLog.updateResult("Success"); // Update the log result to "Success"
+    res.cookie('token', token, { httpOnly: true}); // Store token in cookie
     res.status(200).json({ message: 'Login successful' });
-    // Update authLog to success and save
-    authLog.result = "Success";
     
 
   } catch (error) {
      // Log the failure
     res.status(500).json({ error: "Failed to login user", message: error.message });
   }
-
-  finally {
-    await authLog.save();
-  }
 };
 
 // Logout
 const logout = async (req, res) => {
 
-  const authLog = new authLogs({
-    username: req.body.username || "Unknown", // Use provided username or "Unknown" if undefined
-    type: "Logout",
-    result: "Failure", // Default to "Failure" and update later if successful
-  });
-
+  const authLog = new authLogs();
+  await authLog.initializeAndSave(req.user.username, "Logout"); // Initialize and save the log
   try {
     res.clearCookie('token');
+    authLog.updateResult("Success"); // Update the log result to "Success"
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     res.status(500).json({ error: error.message });
