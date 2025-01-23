fd25432f8f1bbde17cfe1c2fb109aef9b83c5509
darf10 <118679710+darf10@users.noreply.github.com>
2025-01-23 16:58:06 +0200

    Camera

---
+++ backend/models/Camera.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+// Camera Schema
+const cameraSchema = new Schema({
+  cameraId: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  activeAccidents: [{
+    type: Schema.Types.ObjectId,
+    ref: "Accident",
+  }],
+  accidentHistory: [{
+    type: Schema.Types.ObjectId,
+    ref: "Accident",
+  }],
+  users: [{
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    }],
+});
+
+const Camera = model("Camera", cameraSchema);
+
+module.exports = Camera;
