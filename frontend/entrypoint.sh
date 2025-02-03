0b1a557fb1d6ff3bcd51d5681a8723d3b74049e6
Nir Hazan <nirhazan7@gmail.com>
2025-02-03 19:31:51 +0200

    Refactor Dockerfile and entrypoint script for Nginx configuration; update proxy_pass syntax for backend URLs

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '$BACKEND_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g 'daemon off;'
