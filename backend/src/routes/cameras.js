0f98407087abdf03b355e01943387d3160edae11
darf10 <118679710+darf10@users.noreply.github.com>
2025-05-12 15:11:49 +0300

    add new camera page added

--- backend/src/routes/cameras.js
+++ backend/src/routes/cameras.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { verifyToken, hasPermission} = require('../middleware/auth');
-const { getCameras, getLocations, assignCameras } = require('../controllers/cameras')
+const { getCameras, getLocations, assignCameras, addNewCamera } = require('../controllers/cameras')
 
 const router = express.Router();
 
 router.get("/get-cameras" , verifyToken, getCameras);
 router.get("/get-id_location", verifyToken, getLocations);
 router.post("/assign-cameras" , verifyToken, hasPermission("admin"), assignCameras);
+router.post("/add-new-camera", verifyToken, hasPermission("admin"), addNewCamera);
 
 
 
